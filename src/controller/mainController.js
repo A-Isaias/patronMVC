003a212011f2d2034776e3a24c05b7cafe1848be
David Lescano <lescanodavid07@gmail.com>
2023-08-04 12:20:15 -0300

    filterSearch

--- src/controller/mainController.js
+++ src/controller/mainController.js
@@ -1,13 +1,42 @@
 const path = require('path');
 const productData = require('../productData.json');
 
-
-
 const mainController = {
-    home: (req, res) =>{
+    home: (req, res) => {
         const data = productData.results
 
-        res.render('home',{ title: 'Mercado  Liebre', data: data })},
+        res.render('home', {
+            title: 'Mercado  Liebre',
+            data: data
+        })
+    },
+
+    search: (req, res) => {
+        const {
+            q
+        } = req.query;
+
+        const {
+            results
+        } = productData
+
+        if (q) {
+
+            const listSearch = results.filter(({
+                title
+            }) => title.includes(q.toLowerCase()))
+
+            res.render('home', {
+                title: 'Mercado Liebre',
+                data: listSearch
+            })
+        }
+
+        res.render('home', {
+            title: 'Mercado  Liebre',
+            data: results
+        })
+    }
 }
 
 module.exports = mainController;
--- src/routes/main.js
+++ src/routes/main.js
@@ -3,6 +3,8 @@ const route = express.Router();
 const mainController = require('../controller/mainController');
 
 /* "/" */
-route.get('/', mainController.home );
+route.get('/', mainController.search );
+// route.get('/', mainController.home );
+
 
 module.exports = route;
